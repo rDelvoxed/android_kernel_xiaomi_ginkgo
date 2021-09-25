031f4e6e0d47543b5308fcf566fcfd90e1231b91
fjunio97 <flaviojunio1997@gmail.com>
2021-09-25 05:55:21 -0300

    Revert "Rename Kernel Name"
    This reverts commit 8458b558e12afdb97786941254ace83b45d1114d.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="QuickSilver-v2.5-ginkgo-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="QuicksilveRV2-ginkgo-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$HOME/tc/azure-clang"
 AK3_DIR="$HOME/android/AnyKernel3"
 DEFCONFIG="vendor/ginkgo-perf_defconfig"
